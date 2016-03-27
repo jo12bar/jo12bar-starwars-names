5b67657570974a5a6640c0a43afb3d1e40e7b5aa
jo12bar <jo12bar@users.noreply.github.com>
2016-03-27 16:41:25 -0700

    Finish unit tests for index.js

--- test/index.js
+++ test/index.js
@@ -2,7 +2,24 @@ const expect = require('chai').expect;
 const starWars = require('../src/index');
 
 describe('jo12bar-starwars-names', function () {
-  it('should work!', function () {
-    expect(true).to.be.true;
+  describe('all', () => {
+    it('should be an array of strings', () => {
+      expect(starWars.all).to.satisfy(isArrayOfStrings);
+
+      function isArrayOfStrings(array) {
+        return array.every(item => typeof item === 'string');
+      }
+    });
+
+    it('should contain "Luke Skywalker"', () => {
+      expect(starWars.all).to.include('Luke Skywalker');
+    });
   });
+
+  describe('random', () => {
+    it('should return a random item from the starWars.all', () => {
+      const randomItem = starWars.random();
+      expect(starWars.all).to.include(randomItem);
+    });
+  })
 });
