809955f46b176aafbf7a3a265ec9092f904a4136
jo12bar <jo12bar@users.noreply.github.com>
2016-03-27 16:24:38 -0700

    Begin adding unit tests w/ mocha & chai

---
+++ test/index.js
@@ -0,0 +1,8 @@
+const expect = require('chai').expect;
+const starWars = require('../src/index');
+
+describe('jo12bar-starwars-names', function () {
+  it('should work!', function () {
+    expect(true).to.be.true;
+  });
+});
