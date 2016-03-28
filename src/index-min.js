68bfa2ab0f3fd0d818741c408453c72b0a69f1fd
jo12bar <jo12bar@users.noreply.github.com>
2016-03-27 17:34:51 -0700

    Remove minified versions of index.js
    After some research, it seems that it would be better to leave index.js
    un-minified. Bandwidth is not an issue when it comes to server-side
    javascript (node.js), and people using this package on the web will
    probably be using webpack, browserify, etc... Also, this library is so
    small that adding an extra build step is probably not worth it.
    
    Still though, thanks @MaXwellFalstein! You reminded me to minify the
    json... :)

--- src/index-min.js
+++
@@ -1,7 +0,0 @@
-const uniqueRandomArray = require('unique-random-array');
-const starWarsNames = require('./starwars-names.min.json');
-
-module.exports = {
-  all: starWarsNames,
-  random: uniqueRandomArray(starWarsNames),
-};
